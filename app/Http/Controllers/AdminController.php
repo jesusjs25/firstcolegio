b132adf731176bd0be7c243a7652c68c96bd0fa6
Jose Manuel <hernandezjgr12@gmail.com>
2026-04-15 19:27:15 -0400

    Documentacion

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,15 +8,23 @@ use App\Models\Course;
 use App\Models\Subject;
 use Illuminate\Http\Request;
 
+/**
+    * Gestion de funciones administrativas
+    ** Este controlador maneja las funciones administrativas del sistema, como la visualización de estadísticas y la gestión de usuarios.
+    *  Se encarga de recopilar datos relevantes para el administrador y presentarlos en la vista correspondiente.
+*/
+
 class AdminController extends Controller
 {
     public function admin()
     {
+    // Estadísticas generales de estudiantes, profesores, cursos y materias
         $totalStudents = Student::count();
         $totalTeachers = Teacher::count();
         $totalCourses = Course::count();
         $totalSubjects = Subject::count();
 
+    // Retornar la vista con las estadísticas y los datos necesarios para el dashboard
         return view('admin.index', compact(
             'totalStudents',
             'totalTeachers',
